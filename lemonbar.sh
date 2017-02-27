23815bf9335b3aed45386912ac29b71e5b599de1
christie <bobcatfish@gmail.com>
2017-02-27 09:05:39 -0800

    Add memory and CPU usage to lemonbar
    Based on this sweet tutorial:
    http://blog.z3bra.org/2014/04/meeting-at-the-bar.html

--- lemonbar.sh
+++ lemonbar.sh
@@ -1,20 +1,32 @@
 #!/usr/bin/bash
 
-# Define the clock
 Clock() {
-        DATETIME=$(date "+%a %b %d, %T")
-
-        echo -n "$DATETIME"
+    DATETIME=$(date "+%a %b %d, %T")
+    echo -n "$DATETIME"
 }
 
-#Define the battery
 Battery() {
-        BATPERC=$(acpi --battery | cut -d, -f2)
-        echo "$BATPERC"
+    BATPERC=$(acpi --battery | cut -d, -f2)
+    echo "$BATPERC"
 }
+
+CPU() {
+    CPUUSAGE=$(ps -eo pcpu | awk 'BEGIN {sum=0.0f} {sum+=$1} END {print sum}')
+    echo "$CPUUSAGE"
+}
+
+RAM() {
+    # store the total and free memory in two variables
+    read t <<< `grep -E 'MemTotal' /proc/meminfo |awk '{print $2}'`
+    read a <<< `grep -E 'MemAvailable' /proc/meminfo |awk '{print $2}'`
+
+    # then, calcultate the percentage of memory used
+    RAMUSAGE=$(bc <<< "scale=2; 100 * (($t-$a) / $t)")
+    echo "$RAMUSAGE"
+}
+
 
 while true; do
-        #echo "%{r}$(Battery)"
-        echo "%{c}%{F#FFFF00}%{B#FF00FF} $(Clock) %{r}$(Battery)"
-        sleep 1
+    echo "%{l}CPU: $(CPU)% Memory: $(RAM)%  %{c}%{F#FFFF00}%{B#FF00FF} $(Clock) %{r}Battery:$(Battery)"
+    sleep 1
 done
